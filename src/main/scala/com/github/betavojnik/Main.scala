2c1148c586034f04656313275f54def6f83f1217
mvelimir <velimirmilinkovic1@gmail.com>
2024-06-18 01:23:22 +0200

    Run linter

--- src/main/scala/com/github/betavojnik/Main.scala
+++ src/main/scala/com/github/betavojnik/Main.scala
@@ -1,10 +1,9 @@
 package com.github.betavojnik
 
-import org.apache.pekko.actor.typed.scaladsl.Behaviors
 import org.apache.pekko.actor.typed.ActorSystem
+import org.apache.pekko.actor.typed.scaladsl.Behaviors
 
 object Main {
-  def main(args: Array[String]): Unit = {
-    val system: ActorSystem[Any] = ActorSystem(Behaviors.empty, "FederatedLearning")
-  }
+  def main(args: Array[String]): Unit =
+    ActorSystem(Behaviors.empty, "FederatedLearning")
 }
