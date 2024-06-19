15f9dc2ca330936698340c88c17e38de5811a005
mvelimir <velimirmilinkovic1@gmail.com>
2024-06-19 23:27:26 +0200

    Set up cluster

---
+++ src/main/scala/com/github/betavojnik/actors/CoordinatorActor.scala
@@ -0,0 +1,32 @@
+package com.github.betavojnik.actors
+
+import org.apache.pekko.actor.typed.Behavior
+import org.apache.pekko.actor.typed.scaladsl.Behaviors
+
+object CoordinatorActor {
+  sealed trait Data
+  final case class LocalModelFromTrainer(model: Any)             extends Data
+  private final case class LocalModelFromCoordinator(model: Any) extends Data
+
+  /**
+   * Creates an instance of a coordinator actor for exchanging local models with other coordinators
+   */
+  def apply(): Behavior[Data] =
+    Behaviors.setup { _ =>
+      listening()
+    }
+
+  def listening(): Behavior[Data] =
+    Behaviors.receive { (ctx, msg) =>
+      msg match {
+        case LocalModelFromCoordinator(_) =>
+          Behaviors.same
+        case LocalModelFromTrainer(_) =>
+          Behaviors.same
+        case _ =>
+          ctx.log.debug("Message received")
+
+          Behaviors.same
+      }
+    }
+}
